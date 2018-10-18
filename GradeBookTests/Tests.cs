28f50aa3aa93306ccde56b22c649f1a45aa37e4b
ksulo <kledion.sulo@hyland.com>
2018-10-18 11:16:38 -0400

    changes 10/18/2018 11:16AM - Arrays (last lesson)

--- GradeBookTests/Tests.cs
+++ GradeBookTests/Tests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Grades;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
 
 namespace GradeBookTests
 {
@@ -38,6 +40,84 @@ namespace GradeBookTests
 
         }
 
+        [Test]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
 
+        [Test]
+        public void GradeBookVariableHolder()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+            g1 = new GradeBook();
+            g1.Name = "Scott's great book";
+            Assert.AreNotEqual(g1.Name, g2.Name);
+        }
+
+        [Test]
+        public void TypeTest()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+            bool result = string.Equals(name1, name2, System.StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestValues()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+            GiveBookAName(book2);
+            Assert.AreEqual("A grand Book", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A grand Book";
+        }
+
+        [Test]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number = number + 1;
+        }
+
+        [Test]
+        public void VerifyTrim()
+        {
+            string book = " my book ";
+            book = book.Trim();
+            Assert.AreEqual(book, "my book");
+        }
+
+        [Test]
+        public void AddDaysTime()
+        {
+            DateTime date = new DateTime(2018, 10, 18);
+            date = date.AddDays(1);
+            Assert.AreEqual(19, date.Day);
+
+        }
+        
+        [Test]
+        public void UpperCaseString()
+        {
+           string name = "kley";
+           name = name.ToUpper();
+           Assert.AreEqual("KLEY", name);
+        }
     }
-}
+} 
